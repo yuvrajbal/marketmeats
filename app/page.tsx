b38e732f790ac647b004f95f45c60c1a2a21d089
Yuvraj Bal <baly@mcmaster.ca>
2025-04-13 21:18:32 +0530

    landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,182 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <></>
+    <main>
+      <Hero />
+      <TheDifferenceSection />
+      <VariationsSection />
+      <VisualAllItems />
+    </main>
+  );
+}
+
+function Hero() {
+  return (
+    <main className="relative overflow-x-visible">
+      <div className="relative w-full h-screen">
+        <Image
+          src="/home-hero.webp"
+          alt="hero"
+          fill
+          className="object-cover w-full h-full lg:w-full lg:h-fit md:object-center object-right"
+          priority
+        />
+
+        {/* Text Overlay */}
+        <div className="absolute inset-0 flex flex-col items-center justify-center gap-6 md:gap-12 text-white uppercase z-10 p-2 ">
+          <h1 className="text-center font-sans  text-5xl/10 md:text-7xl/16 max-w-2xl md:max-w-5xl font-bold tracking-tighter  drop-shadow-xl">
+            "IF IT'S NOT RAISED RIGHT IT CAN'T BE DELICIOUS"
+          </h1>
+          <div className=" flex sm:flex-row flex-col sm:justify-between md:w-4xl  gap-4 font-semibold md:font-bold text-sm md:text-lg tracking-widest">
+            <p className="drop-shadow-xl">no hormones</p>
+            <p className="drop-shadow-xl">no antibiotics</p>
+            <p className="drop-shadow-xl">pasture-raised</p>
+          </div>
+          <button className="w-6/7 sm:w-48 mt-6  py-4 bg-neutral-800 text-white  font-bold hover:bg-white hover:text-black hover:border hover:border-gray-400 transition uppercase tracking-wide">
+            Shop Now
+          </button>
+        </div>
+      </div>
+    </main>
+  );
+}
+
+function TheDifferenceSection() {
+  return (
+    <section className="bg-gray-50 py-12 ">
+      <div className="max-w-5xl p-4 mx-auto">
+        <div className="border-b border-b-gray-800  flex flex-col items-center justify-center gap-6 mx-auto py-8 text-center">
+          <h1
+            className="max-w-sm  
+            uppercase 
+            text-black 
+            font-bold text-xl md:text-2xl/6 md:font-extrabold md:tracking-normal tracking-tighter"
+          >
+            Now delivering meat the way meat should be
+          </h1>
+          <p className="text-gray-800 font-sans text-base ">
+            By sourcing from pastures in Vancouver, and dry aging and
+            hand-cutting it at our own facilities, we’re setting a new standard
+            for meat. After years of serving our local community, we’re now
+            delivering nationwide. Once you taste the difference, you’ll never
+            go back.
+          </p>
+        </div>
+        <div className="flex flex-col md:flex-row justify-between mt-12">
+          <div className="flex-1 font-sans uppercase tracking-tighter  text-orange-600 font-bold md:font-extrabold md:text-7xl/18 md:tracking-tight text-6xl/12">
+            <h1 className="">Let's </h1>
+            <h1 className="ml-16 md:ml-24">dig in </h1>
+          </div>
+          <div className="flex-1  ">
+            <p className=" mt-12 mb-8">
+              Cooking for two or ten? Firing up the grill or the skillet? How
+              you’re cooking matters, and we want to help you get it right every
+              time. By practicing whole-animal butchery, we’re able to offer
+              tons of incredible cuts and can guide you toward the perfect one
+              for any occasion.
+            </p>
+
+            <Link
+              href={"/learn"}
+              className=" uppercase tracking-wide text-sm font-bold border-b-1 border-b-orange-600 pb-1 "
+            >
+              Learn more about our philosophy
+            </Link>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+function VariationsSection() {
+  return (
+    <section className="grid grid-cols-1 md:grid-cols-4  mx-auto ">
+      <ShopItem
+        mobileSrc={"/skillet-stand-outs_2xrmobile_800x322.jpg"}
+        desktopSrc={"/skillet-stand-outs_2xr_700x1360.jpg"}
+        overlayText="Subscribe & Save"
+      />
+      <ShopItem
+        mobileSrc={"/burger-to-end-all-burgs_2xrmobile_800x322.jpg"}
+        desktopSrc={"/burger-to-end-all-burgers_2xr_700x1360.jpg"}
+        overlayText="Dry aged burgers"
+      />
+      <ShopItem
+        mobileSrc={"/grill-go-tos_2xrmobile_800x322.jpg"}
+        desktopSrc={"/grill-go-tos_2xr_700x1360.jpg"}
+        overlayText="grill go-tos"
+      />
+      <ShopItem
+        mobileSrc={"/cuts-for-a-crowd_2xrmobile_800x322.jpg"}
+        desktopSrc={"/cuts-for-a-crowd_2xr_700x1360.webp"}
+        overlayText="cuts for the crowd"
+      />
+    </section>
+  );
+}
+
+interface ShopItemProps {
+  mobileSrc: string;
+  desktopSrc: string;
+  overlayText: string;
+}
+function ShopItem({ mobileSrc, desktopSrc, overlayText }: ShopItemProps) {
+  return (
+    <div className="relative w-full h-[171px] sm:h-[700px]">
+      {/* Mobile Image */}
+      <Image
+        src={mobileSrc}
+        alt={overlayText}
+        fill
+        className="object-cover block sm:hidden"
+      />
+
+      {/* Desktop Image */}
+      <Image
+        src={desktopSrc}
+        alt={overlayText}
+        fill
+        className="object-cover hidden sm:block"
+      />
+
+      {/* Centered Text */}
+      <div className="absolute inset-0 flex items-center justify-center">
+        <h2 className="uppercase text-white text-xl font-bold text-center drop-shadow-lg">
+          {overlayText}
+        </h2>
+      </div>
+    </div>
+  );
+}
+
+function VisualAllItems() {
+  return (
+    <main className="max-w-6xl mx-auto py-16">
+      <div className="relative">
+        <Image
+          src={"/Box_Overhead-0081-Edit_lo_1880x1050.webp"}
+          alt="all items"
+          height={1050}
+          width={1880}
+          className="w-full h-auto"
+        />
+        <div className="absolute bottom-[-250px] left-1/2 transform -translate-x-1/2">
+          <Image
+            src={
+              "/Inspirational_Glass_of_Whiskey_61814cff-55d9-48dc-ac72-452216e63819_1060x720.webp"
+            }
+            alt="cheers"
+            height={420}
+            width={420}
+          />
+          <h1 className="mt-2 text-center uppercase text-2xl font-bold text-black tracking-wide">
+            Cheers!
+          </h1>
+        </div>
+      </div>
+    </main>
   );
 }
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,35 +1,126 @@
+"use client";
 import Link from "next/link";
 import Image from "next/image";
+import { Menu, ShoppingBasket, X } from "lucide-react";
+import { useState } from "react";
 
 export default function Header() {
+  const [openMenu, setOpenMenu] = useState(false);
+
+  const handleMenuClick = () => {
+    setOpenMenu(!openMenu);
+  };
   return (
-    <nav className="flex flex-row justify-between font-semibold py-4">
-      <div className="flex gap-2 uppercase ">
+    <>
+      <nav className="flex flex-row justify-between font-semibold tracking-widest font-mono p-4">
+        {openMenu ? (
+          <X className="size-10" onClick={handleMenuClick} />
+        ) : (
+          <Menu className="size-10" onClick={handleMenuClick} />
+        )}
+
+        {/* <div className="flex gap-2 uppercase ">
         <NavLink text="shop" href="/shop" />
         <NavLink text="Search" href="/search" />
 
         <NavLink text="best sellers" href="/best-sellers" />
-      </div>
+      </div> */}
 
-      <Image src="/MM_Logo.png" alt="MM Logo" height={80} width={280} />
-      <div className="flex gap-2 uppercase">
+        <Image src="/MM_Logo.png" alt="MM Logo" height={40} width={200} />
+        {/* <div className="flex gap-2 uppercase">
         <NavLink text="about us" href="/about" />
         <NavLink text="tips and tricks" href="/tips-and-tricks" />
         <NavLink text="log in" href="/login" />
         <NavLink text="cart" href="/cart" />
+      </div> */}
+        <ShoppingBasket className="size-10" />
+      </nav>
+      {openMenu && <MobileMenu />}
+    </>
+  );
+}
+
+const MobileMenu = () => {
+  return (
+    <nav className="uppercase">
+      <div className="flex flex-col text-center p-4 bg-gray-50 ">
+        <input
+          type="text"
+          className="font-extrabold text-2xl text-orange-600
+        placeholder:text-orange-600 placeholder:font-extrabold text-center
+        uppercase outline-none tracking-normal"
+          placeholder="How can we help?"
+        />
+        <div className="text-gray-500 font-semibold text-xs tracking-wide ">
+          Type to begin your search
+        </div>
+      </div>
+      <div className="flex flex-col text-center ">
+        <div className="flex flex-col gap-1 mb-3">
+          <NavLink text="shop" href="/shop" />
+          <div className="grid grid-cols-2 gap-2 ">
+            <ShopItem imageSrc={"/head-of-beef.svg"} title="Beef" />
+            <ShopItem imageSrc={"/head-of-pork.svg"} title="Pork" />
+            <ShopItem imageSrc={"/Market_Mobile.webp"} title="Market" />
+            <ShopItem imageSrc={"/head-of-chicken.svg"} title="Chicken" />
+          </div>
+        </div>
+
+        <NavLink
+          text="best sellers"
+          href="/best-sellers"
+          className="border-b border-gray-200 pb-4"
+        />
+        <NavLink
+          text="out story"
+          href="/our-story"
+          className="border-b border-gray-200 pb-4"
+        />
+        <NavLink
+          text="Our Process"
+          href="/our-process"
+          className="border-b border-gray-200 pb-4"
+        />
+        <NavLink
+          text="tips and tricks"
+          href="/tips-and-tricks"
+          className="border-b border-gray-200 pb-4"
+        />
+        <NavLink
+          text="log in"
+          href="/login"
+          className="border-b border-gray-200 pb-4"
+        />
       </div>
     </nav>
+  );
+};
+
+interface ShopItemProps {
+  imageSrc: string;
+  title: string;
+}
+function ShopItem({ imageSrc, title }: ShopItemProps) {
+  return (
+    <div className="flex flex-col items-center justify-between bg-orange-50 text-orange-600 font-bold text-xl p-4">
+      <Image src={imageSrc} alt={title} height={80} width={80} />
+      {title}
+    </div>
   );
 }
 
 interface LinkProps {
   text: string;
   href: string;
+  className?: string;
 }
 
-function NavLink({ text, href }: LinkProps) {
+function NavLink({ text, href, className }: LinkProps) {
   return (
-    <Link href={href} className="hover:text-gray-600 transition-colors p-4">
+    <Link
+      href={href}
+      className={`hover:text-gray-600 text-xs transition-colors p-4 font-bold tracking-wider ${className} `}
+    >
       {text}
     </Link>
   );
